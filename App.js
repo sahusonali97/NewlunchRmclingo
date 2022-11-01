0aa2022926d518e2979a2cae25f3c2d9afc3a571
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2022-11-01 17:59:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Contact from './routes/Contact';
+
+import Home from './routes/Home'
+import Pricing from './routes/Pricing'
+import Training from './routes/Training';
+import Language from './routes/Training';
+
+
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/training' element={<Training />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/Language' element={<Language />} />
+
+      
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
