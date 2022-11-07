4ad6c33e9748df4713bf0cb22099be74e929d870
sahusonali97 <113846876+sahusonali97@users.noreply.github.com>
2022-11-07 12:20:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,13 +2,10 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Contact from './routes/Contact';
 
+import About from './routes/About'
 import Home from './routes/Home'
 import Pricing from './routes/Pricing'
 import Training from './routes/Training';
-import Language from './routes/Training';
-
-
-
 
 function App() {
   return (
@@ -18,10 +15,7 @@ function App() {
         <Route path='/pricing' element={<Pricing />} />
         <Route path='/training' element={<Training />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/Language' element={<Language />} />
-
-      
-
+        <Route path='/About' element={<About/>} />
       </Routes>
     </>
   );
